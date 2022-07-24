5e0ab6c67def3b2fb674e1b023eaaeb9ed3ef431
Kirill <kenny27041994@gmail.com>
2022-07-25 00:37:26 +0300

    add(search):add search, footer, fixed the code

--- src/pages/GoodsPage/GoodsPage.jsx
+++ src/pages/GoodsPage/GoodsPage.jsx
@@ -1,56 +1,7 @@
-import React from 'react';
-import { useSelector, useDispatch } from "react-redux";
-import { useEffect } from 'react';
-import { goodsSelectors } from '../../store/good/';
-import { getFetchGoods } from '../../store/good/slice';
-import { getFetchCategories } from '../../store/categories/slice';
-import { Table } from 'antd';
+import React from 'react'
 
-
-export function GoodsPage () {
-    const dispatch = useDispatch();
-    const { goods } = useSelector(goodsSelectors.getMapGoods)
-
-    useEffect(() => {
-        dispatch(getFetchGoods())
-        dispatch(getFetchCategories())
-    }, [dispatch])
-
-    const columns = [
-        {
-            title: '№',
-            dataIndex: 'id',
-            key: 'id'
-        },
-        {
-            title: 'Category',
-            dataIndex: 'categoryLabel',
-            key: 'categoryTypeId'
-        },
-        {
-            title: 'Goods',
-            dataIndex: 'label',
-            key: 'id',
-            render: (label) => {
-                return <a>{label}</a>
-            }
-        },
-        {
-            title: 'Description',
-            dataIndex: 'description',
-            key: 'id'
-        },
-        {
-            title: 'Price',
-            dataIndex: 'price',
-            key: 'id'
-        }
-    ]
-    return (
-            <Table 
-                dataSource={goods} 
-                columns={columns} 
-                pagination={{ total: 220, pageSize: 10 }}>
-            </Table>
-    )
-} 
+export const GoodsPage = () => {
+  return (
+    <div>GoodsPage</div>
+  )
+}
